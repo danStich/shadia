aa6cd01f91be1a3c5546b8716961792698f54032
danStich <daniel.stich@oneonta.edu>
2021-07-12 09:57:07 -0400

    rebuild under R v4.1.0

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // tempC
 NumericMatrix tempC(NumericVector days, NumericVector years, NumericMatrix coeffs);
 RcppExport SEXP _shadia_tempC(SEXP daysSEXP, SEXP yearsSEXP, SEXP coeffsSEXP) {
